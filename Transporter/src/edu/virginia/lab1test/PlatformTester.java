df81c711a1f9914dc77a5d434204815f947149c7
Nick Onley <nick.onley@aol.com>
2018-07-03 21:23:59 -0400

    Created develop branch. Made PlatformTester extend TransporterGame

--- Transporter/src/edu/virginia/lab1test/PlatformTester.java
+++ Transporter/src/edu/virginia/lab1test/PlatformTester.java
@@ -9,13 +9,14 @@ import java.util.ArrayList;
 import edu.virginia.engine.display.AnimatedSprite;
 import edu.virginia.engine.display.Game;
 import edu.virginia.engine.display.Sprite;
+import edu.virginia.engine.display.TransporterGame;
 import edu.virginia.engine.util.GameClock;
 
 /**
  * Example game that utilizes our engine. We can create a simple prototype game with just a couple lines of code
  * although, for now, it won't be a very fun game :)
  * */
-public class PlatformTester extends Game implements MouseListener{
+public class PlatformTester extends TransporterGame implements MouseListener{
 
 	/* Create a sprite object for our game. We'll use mario */
 	static AnimatedSprite mario = new AnimatedSprite("mario_", 6, false);
